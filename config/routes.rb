af5b1bf6eb06dcb4f14b3e56619d985ea0f4bce6
Jasmin <ssongnari@gmail.com>
2023-03-11 21:27:32 -0500

    commit tree 4f71ca5808265ee3fa8b384736b76eb2e0a02888

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  destroy '/logout', to 'sessions#destroy'
+  get '/me', to: 'users#show'
+  resources :recipes, only: [:index, :create]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
